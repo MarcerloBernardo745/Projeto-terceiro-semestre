36b89c71069fded998a6f9521619dea7301567fe
MarcerloBernardo745 <62345542+MarcerloBernardo745@users.noreply.github.com>
2021-02-24 17:18:42 -0300

    Add files via upload

---
+++ projeto-3-sistemas-de-cardapio/index.php
@@ -0,0 +1,13 @@
+<?php  
+$comprar = array();
+$pedidos = array();
+$tempo_de_entrega = array();
+$avaliação = array();
+$promoções = array();
+
+$comprar = array("Formas de Pagamento" => "Cartão de Crédito", "Quantidade" => "1", "Nome do Produto" => "Coca Cola");
+$avaliação = array("Número de comentários" => "20", "Positivos" => 17, "Negativos" => 3);
+$tempo_de_entrega = array("Região" => "Amapá", "tráfego" => "Ruim", "Numero de paradas" => 3, "Cancelou" => false);
+$promoções = array("Banner Promocional" => "conteudo", "Valor da promoção" => "20% de desconto", "Público alvo" => "Todos");
+$pedidos = array('Código do produto' => "Codigo", 'quantidade' => 2, "Nome do Produto" => "pizza")
+?>
